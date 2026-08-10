4ac150d073ce203569fc319a97dbf3d12095d3f1
zhifengjin050-arch <zhifengjin050@gmail.com>
2026-08-11 07:08:47 +0800

    feat: add source files (batch 54/174)

---
+++ api/internal/adgateway/url_local.go
@@ -0,0 +1,5 @@
+//go:build !release
+
+package adgateway
+
+const ServerURL = "http://localhost:9098"
---
+++ api/internal/adgateway/url_release.go
@@ -0,0 +1,5 @@
+//go:build release
+
+package adgateway
+
+const ServerURL = "https://ads-gateway.dujiao-next.com"
